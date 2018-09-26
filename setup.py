7ce71934b12eb50461bf5a1f6bda400bd273d63d
mahitha-raj <mahiraj@tamu.edu>
2018-09-26 15:02:46 -0500

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.6
+
+from setuptools import setup
+
+setup(name='demo1',
+      version='1.0.0',
+      description='just a farm story',
+      url='http://github.com/mahitha-raj/demo1',
+      author='Mahitha Rajendran',
+      author_email='mahiraj@tamu.edu',
+      license='MIT',
+      packages=['demo1'])
